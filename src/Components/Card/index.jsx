43a54dcfe8c8af5f850552988572af8b99f7d2df
luisvls <luisvls88@gmail.com>
2024-03-14 12:21:29 -0300

    Contador de productos en el carrito

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,4 +1,8 @@
+import { useContext } from "react";
+import { ShoppingCartContext } from "..//..//Context";
+
 const Card = (props) => {
+  const context = useContext(ShoppingCartContext);
   return (
     <div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
       <figure className="relative mb-2 w-full h-4/5">
@@ -10,7 +14,10 @@ const Card = (props) => {
           src={props.data?.image}
           alt={props.data?.title}
         />
-        <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1">
+        <div
+          className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1"
+          onClick={() => context.setCount(context.count + 1)}
+        >
           +
         </div>
       </figure>
