8f0108fbbc15c093909af343c28cd6542c827725
luisvls <luisvls88@gmail.com>
2024-03-14 12:01:05 -0300

    Contexto global de la aplicación

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,3 +1,7 @@
 import { createContext } from "react";
 
 const ShoppingCartContext = createContext();
+
+export const ShoppingCartProvider = ({ children }) => {
+  return <ShoppingCartContext.Provider>{children}</ShoppingCartContext.Provider>;
+};
